f0bc84088a7f742627a2799ccdbe40c74a166872
Jang Yu Jin <wkddbwls145@gmail.com>
2017-04-11 19:24:47 +0830

    #13 각 버튼 누르면 사진 바뀜 구현

--- app/src/main/java/kr/hs/emirim/wkddbwls145/chasooyeon/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/wkddbwls145/chasooyeon/MainActivity.java
@@ -24,18 +24,23 @@ public class MainActivity extends AppCompatActivity {
         switch(view.getId() ){
             case R.id.button1:
                 Log.d(TAG, "버튼1 클릭");
+                mImageViewFriendVisual.setImageResource(R.drawable.photo1);
                 Toast.makeText(this, "애들아 펜타곤 신곡 나왔어!! 들어~~", Toast.LENGTH_SHORT).show();break;
             case R.id.button2:
                 Log.d(TAG, "버튼2 클릭");
+                mImageViewFriendVisual.setImageResource(R.drawable.photo2);
                 Toast.makeText(this, "애들아 블락비 신곡 나왔어!! 들어~~", Toast.LENGTH_SHORT).show();break;
             case R.id.button3:
                 Log.d(TAG, "버튼3 클릭");
+                mImageViewFriendVisual.setImageResource(R.drawable.photo3);
                 Toast.makeText(this, "하..진호 너무 잘생겼어..(감탄)", Toast.LENGTH_SHORT).show();break;
             case R.id.button4:
                 Log.d(TAG, "버튼4 클릭");
+                mImageViewFriendVisual.setImageResource(R.drawable.photo4);
                 Toast.makeText(this, "뭐가?", Toast.LENGTH_SHORT).show();break;
             case R.id.button5:
                 Log.d(TAG, "버튼5 클릭");
+                mImageViewFriendVisual.setImageResource(R.drawable.photo5);
                 Toast.makeText(this, "(웃긴표정을 지으며 입을 살짝벌리며 나를 바라봄)", Toast.LENGTH_SHORT).show();break;
             default:
                 Log.d(TAG, "모르는 녀석 클릭");
